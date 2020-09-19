1ac27a5a3343a870fdef7b06a04456c7c479b9ac
Akshata <67673225+Akshata-Mandloi@users.noreply.github.com>
2020-09-19 12:21:30 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,28 +1,30 @@
-# cresting a class
+# creating a class
 class Difft:
-
+# 	method m1 for taking integers in first array
 	def m1(self): 
 		for i in range(NoOfEle):
 			i=int(input('enter elements'))
 			list1.append(i)
 			print(list1)
-	
+			
+# 	method m2 for taking integers in second array
 	def m2(self):
 		for j in range(NoOfEle):
 			j=int(input('enter elements'))
 			list2.append(j)
 			print(list2)
-	
+			
+# 	chomparing the array is same or not
 	def sorting(self):
 		list1.sort()
 		list2.sort()
 		if list1==list2:
-			print("equal")
+			print("All emements are same")
 		else:
-			print("not equal")
+			print("not all elements are same")
 
 
-
+# calling the method via object created for class difft
 
 list1=[]
 list2=[]
